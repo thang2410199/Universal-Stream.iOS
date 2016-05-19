feb46a73ad96ad654ff09b44807f8a069e4d8e52
Ngo Quoc Thang <thang2410199@gmail.com>
2016-05-19 10:34:23 +0300

    Initial Commit

---
+++ Suto Stream/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Suto Stream
+//
+//  Created by Ngo Quoc Thang on May/19/16.
+//  Copyright © 2016 Ngo Quoc Thang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
